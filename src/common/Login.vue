3867f42aa7675e94eaed01b7064c0b274f4f24a3
velimir-97 <velimirr97@gmail.com>
2019-09-14 17:02:00 +0200

    Fixed forms not working properly

--- src/common/Login.vue
+++ src/common/Login.vue
@@ -6,40 +6,38 @@
 					<div class="card">
 						<div class="card-header">Login</div>
 						<div class="card-body">
-							<form action="" method="">
-								<div class="form-group row">
-									<label for="username" class="col-md-4 col-form-label text-md-right">Username</label>
-									<div class="col-md-6">
-										<input type="text" id="username" class="form-control" name="username" autofocus v-model="username">
-									</div>
+							<div class="form-group row">
+								<label for="username" class="col-md-4 col-form-label text-md-right">Username</label>
+								<div class="col-md-6">
+									<input type="text" id="username" class="form-control" name="username" autofocus v-model="username">
 								</div>
+							</div>
 
-								<div class="form-group row">
-									<label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
-									<div class="col-md-6">
-										<input type="password" id="password" class="form-control" name="password" v-model="password">
-									</div>
+							<div class="form-group row">
+								<label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
+								<div class="col-md-6">
+									<input type="password" id="password" class="form-control" name="password" v-model="password">
 								</div>
-								<p v-if="errorField">Both fields are required</p>
-								<div class="form-group row">
-									<div class="col-md-6 offset-md-4">
-										<div class="checkbox">
-											<label>
-												<input type="checkbox" name="remember"> Remember Me
-											</label>
-										</div>
-									</div>
-								</div>
-
+							</div>
+							<p v-if="errorField">Both fields are required</p>
+							<div class="form-group row">
 								<div class="col-md-6 offset-md-4">
-									<button type="submit" class="btn btn-primary" @click="login()">
-										Login
-									</button>
-									<a href="#" class="btn btn-link">
-										Forgot Your Password?
-									</a>
+									<div class="checkbox">
+										<label>
+											<input type="checkbox" name="remember"> Remember Me
+										</label>
+									</div>
 								</div>
-							</form>
+							</div>
+
+							<div class="col-md-6 offset-md-4">
+								<button class="btn btn-primary" @click="login()">
+									Login
+								</button>
+								<a href="#" class="btn btn-link">
+									Forgot Your Password?
+								</a>
+							</div>
 						</div>
 					</div>
 				</div>
