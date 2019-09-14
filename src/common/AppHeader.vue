d5c185936dc4aa4d1150592cd63f04ee338ea62b
velimir-97 <velimirr97@gmail.com>
2019-09-14 18:20:05 +0200

    FIxed hamburger menu not working

--- src/common/AppHeader.vue
+++ src/common/AppHeader.vue
@@ -1,11 +1,11 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
   <router-link class="navbar-brand" to="/">Family Tree</router-link>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" v-on:click="toggleNavbar">
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent" v-bind:class="{'show': show}">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item">
         <router-link class="nav-link" to="/">Home</router-link>
@@ -45,7 +45,8 @@ export default {
 
 	data () {
 		return {
-			loggedIn: false
+			loggedIn: false,
+            show: true
 		}
 	},
 	methods: {
@@ -54,7 +55,10 @@ export default {
 			this.loggedIn = false
 			this.$router.push({ name: 'Index'})
 
-		}
+		},
+        toggleNavbar(){
+		    this.show = !this.show
+        }
 	}
 }
 </script>
